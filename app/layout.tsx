a050f63e4f2680a4a08206aa79adce8044b54850
AyushiiiAwasthi <143478386+AyushiiiAwasthi@users.noreply.github.com>
2024-04-26 19:29:12 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Head from "next/head";
+
+
+export const metadata: Metadata = {
+  title: "Hilink",
+  description: "Travel app for camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" /> 
+      </Head>
+      <body >
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
